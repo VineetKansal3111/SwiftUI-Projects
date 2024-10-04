f15376bd984e4c9c98e878fee9c417aa8563a10d
Vineet Kansal <kansal6283@gmail.com>
2024-10-04 13:48:07 +0530

    Initial Commit

---
+++ SwiftThinkingBootcamp/SwiftThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftThinkingBootcampApp.swift
+//  SwiftThinkingBootcamp
+//
+//  Created by IOS on 04/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
